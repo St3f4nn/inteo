be70dbecd071928523e855af7c8e7e1284301364
Stefan <gajicstefan414@gmail.com>
2023-04-16 16:21:39 +0200

    Set up a work environment - create a GitHub repository, add all necessary files and folders (.gitignore, dist, src, etc.), install Tailwind (low-level utility-based CSS framework) and set up the configuration for it (container, colors, fonts, etc.)

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: "rgb(230, 216, 204)",
+          300: "rgb(196, 163, 134)",
+          600: "rgb(153, 104, 48)",
+        },
+        neutral: {
+          250: "rgb(217, 217, 214)",
+          450: "rgb(124, 124, 124)",
+          650: "rgb(68, 68, 68)",
+          750: "rgb(44, 44, 44)",
+        },
+      },
+      fontFamily: {
+        "playfair-display": "'Playfair Display', serif",
+        outfit: "'Outfit', sans-serif",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-debug-screens"),
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "1296px",
+        },
+      });
+    },
+  ],
+};
